4dedfea777ca7f2b24a052709f784f1558b7bd71
Muhammad <phonzammizaki@gmail.com>
2024-12-13 19:43:20 +0700

    add `styled-jsx` usage

--- pages/index/Counter.tsx
+++ pages/index/Counter.tsx
@@ -4,8 +4,19 @@ export function Counter() {
   const [count, setCount] = useState(0);
 
   return (
-    <button type="button" onClick={() => setCount((count) => count + 1)}>
-      Counter {count}
-    </button>
+    <>
+      <button type="button" onClick={() => setCount((count) => count + 1)}>
+        Counter {count}
+      </button>
+
+      <style jsx>{`
+      button {
+        background: teal;
+        color: white;
+        border-radius: 1.5rem;
+        cursor: pointer;
+      }
+    `}</style>
+    </>
   );
 }
